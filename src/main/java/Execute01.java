27eb10810b9c46353e83f7452f315f3cb064240b
fatayt <fatayt@gmail.com>
2023-01-29 23:30:15 +0300

    JDBC_first

--- src/main/java/Execute01.java
+++ src/main/java/Execute01.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class Execute01 {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Execute01 {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        //1. Adim: Driver'a kaydol
+        Class.forName("org.postgresql.Driver");
+
+        //2.Adim: Database'e baglan
+        Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/arcane", "postgres","2615Fat.");
+
+        //3.Adim: Statement olustur
+        Statement st = con.createStatement();
+
+        System.out.println("Baglanti Basarili");
+
+
+        //4. Adim: Query (sorgu) olustur
+
+        //1: Ornek: "workers" adinda bir table olusturup "worker_id, worker_name, worker_salary" sutunlarini ekleyin
+
+        boolean sql1 = st.execute("Create Table workers(worker_id VARCHAR(20),worker_name VARCHAR(20), worker_salary INT)");
+        System.out.println("sql1 = " + sql1); // false dondurur cunku data cagirmiyoruz
+        /*
+         execute() methodu DDL -data definition language- (create, drop, alter table) ve DQL -data query language- (select) icin kullanabiliriz
+         i) Eger execute() methodu DDL icin kullanilirsa 'false' return eder
+         ii) Eger execute() methodu DQL icin kullanilirsa ResultSet (sonuc/data/veri) alindiginda 'true' aksi halde 'false' dondurur
+         */
+
+        //2. Ornek: Table'a worker_address sutunu ekleyerek alter yapin
+
+        String sql2 = "Alter Table workers Add worker_address VARCHAR(80)";
+        boolean sql2a = st.execute(sql2);
+        System.out.println("sql2a = " + sql2a);
+
+        //3.Ornek: workers table'ini silin
+
+        String sql3 = "Drop Table workers";
+        boolean sql3a = st.execute(sql3);
+        System.out.println("sql3a = " + sql3a);
+
+        //5. Adim: Baglanti ve Statement'i kapat
+
+        con.close();
+        st.close();
+
+    }
 }
--- src/main/java/ExecuteQuery01.java
+++ src/main/java/ExecuteQuery01.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class ExecuteQuery01 {
+import java.sql.*;
+
+public class ExecuteQuery01 {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        //1. Adim: Driver'a kaydol
+        Class.forName("org.postgresql.Driver");
+
+        //2.Adim: Database'e baglan
+        Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/arcane", "postgres","2615Fat.");
+
+        //3.Adim: Statement olustur
+        Statement st = con.createStatement();
+
+        //1. Orn: region id'si 1 olan "country name" degerlerini cagirin
+
+        String sql1 = "SELECT country_name from  countries where region_id=1";
+        boolean sonuc = st.execute(sql1);
+        System.out.println("sonuc = " + sonuc);
+
+        //Record/satirlari gormek icn ExecuteQuery() methodunu kullanmaliyiz
+        ResultSet rS1 = st.executeQuery(sql1);
+        System.out.println("rS1 = " + rS1);
+
+        while (rS1.next()){
+            rS1.getString(1); // veya rS1.getString("country_name");
+            System.out.println(rS1.getString(1));
+        }
+//2. orn: "region_id'nin 2'den buyuk oldugu "country_id" ve "country_name" degerlerini cagirin
+
+    }
 }
